7c2079da388e21d0c62f2dc25205072d822aadb0
mintsweet <0x1304570@gmail.com>
2024-09-24 13:58:12 +1200

    fix: update support file description

--- src/ui/components/file-upload.tsx
+++ src/ui/components/file-upload.tsx
@@ -25,7 +25,7 @@ export const FileUpload = () => {
             <span className="font-semibold">Click to upload</span> or drag and
             drop
           </p>
-          <p className="text-xs text-gray-500 ">SVG, PNG, JPG or GIF</p>
+          <p className="text-xs text-gray-500 ">TXT, WORD, EXCEL</p>
         </div>
         <input id="dropzone-file" type="file" className="hidden" />
       </label>
